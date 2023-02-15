91c3cd9868de8b3f1151f15ed72127fe33101f16
Mmmr7 <ol-sidorenkov@mail.ru>
2023-02-15 22:09:09 +0300

    prod]

--- HW01-PySpark/main-task/main.py
+++ HW01-PySpark/main-task/main.py
@@ -1,54 +1,51 @@
 import pyspark.sql.functions as f
 from pyspark.sql import SparkSession
 
-
 INPUT_PATH: str = 'wnp.txt'
 OUTPUT_PATH: str = 'target'
 
 
-def main():
-    '''
-        Solution based on presentation tutorial
-    '''
+class WordCounter:
+    input_path: str
+    output_path: str
+    spark: SparkSession
 
-    spark: SparkSession = SparkSession.builder.getOrCreate()
+    def __init__(self, input_path: str, output_path: str, spark: SparkSession):
+        self.input_path = input_path
+        self.output_path = output_path
+        self.spark = spark
 
-    input_file = spark.sparkContext.textFile(INPUT_PATH)
-    mp = input_file.flatMap(
-        lambda line: line.split(' ')
-    ).map(
-        lambda word: (word, 1)
-    )
+    def main_solution(self):
+        '''
+            Solution based on presentation tutorial
+        '''
 
-    counts = mp.reduceByKey(lambda a, b: a + b)
-    counts.saveAsTextFile(OUTPUT_PATH)
+        self.spark.sparkContext.textFile(self.input_path).flatMap(
+            lambda line: line.split(' ')
+        ).map(
+            lambda word: (word, 1)
+        ).reduceByKey(
+            lambda count1, count2: count1 + count2
+        ).saveAsTextFile(self.output_path)
 
+    def other_solution(self):
+        '''
+            My custom solution
+        '''
 
-def other():
-    '''
-        My custom solution
-    '''
-        
-    spark: SparkSession = SparkSession \
-        .builder \
-        .getOrCreate()
-
-    spark \
-        .read \
-        .text(INPUT_PATH) \
-        .withColumn(
+        self.spark.read.text(INPUT_PATH).withColumn(
             'word',
             f.explode(
-                f.split(
-                    # Splitting strings and getting separate words
+                f.split(  # Splitting strings and getting separate words
                     f.lower(f.col('value')),
                     ' ',
                 )
             )
         ).filter(  # Filtering empty strings
             f.col('value') != ''
-            # Counting and sorting by count descending
-        ).groupBy('word').count().sort(
+        ).groupBy(
+            'word',
+        ).count().sort(  # Counting and sorting by count descending
             'count',
             ascending=False,
         ).show()
@@ -65,8 +62,14 @@ def super_log(message: str):
 
 
 if __name__ == '__main__':
+    word_counter: WordCounter = WordCounter(
+        INPUT_PATH,
+        OUTPUT_PATH,
+        SparkSession.builder.getOrCreate(),
+    )
+
     super_log('Running main solution')
-    main()
+    word_counter.main_solution()
 
     super_log('Running other solution')
-    other()
+    word_counter.other_solution()
